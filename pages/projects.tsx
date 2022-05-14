7e296048bdabe873a39d38702550f966bc76fc35
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-05-14 10:44:13 +0530

    issue resolved

---
+++ pages/projects.tsx
@@ -0,0 +1,24 @@
+import Button from "@mui/material/Button"
+// import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  
+  return (
+    <>
+      <Layout>
+       
+          <div className={styles.home_container_container}>
+          <div className={styles.home_banner}>
+               <div>
+              
+               </div>
+            </div>
+            
+          </div>
+      
+      </Layout>
+    </>
+  )
+}
