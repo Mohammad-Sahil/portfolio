dc9e537c4f7d6a70ba8f568fbe10ae109e25239a
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-09-18 22:24:00 +0530

    navbar modified

---
+++ components/WorkExperience.tsx
@@ -0,0 +1,12 @@
+import styles1 from '../styles/Session.module.css';
+import styles from '../styles/Experience.module.css';
+
+const WorkExperience = () => {
+  return (
+    <div className={styles1.session_container_div}>
+        Coming Soon
+    </div>
+  )
+}
+
+export default WorkExperience
